4e09ffe5a505e4c58276fa22a1fdb672a2d8dfd9
Jia Jun Lee <lee19@live.cn>
2023-09-02 21:51:35 +0800

    Created black js file

---
+++ Frontend/js/utils.js
@@ -0,0 +1,7 @@
+// Define your api here
+var productListApiUrl = 'http://127.0.0.1:5000/getProduct';
+var uomListApiUrl = 'http://127.0.0.1:5000/getUOM';
+var productSaveApiUrl = 'http://127.0.0.1:5000/insertProduct';
+var productDeleteApiUrl = 'http://127.0.0.1:5000/deleteProduct';
+var orderListApiUrl = 'http://127.0.0.1:5000/getAllOrders';
+var orderSaveApiUrl = 'http://127.0.0.1:5000/insertOrder';
