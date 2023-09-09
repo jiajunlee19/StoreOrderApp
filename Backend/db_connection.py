6b077547d0c20c824370a1f84fcb7e8f986e6c2c
Jia Jun Lee <lee19@live.cn>
2023-09-09 09:39:29 +0800

    Deploy generate_update_statement

--- Backend/db_connection.py
+++ Backend/db_connection.py
@@ -130,7 +130,7 @@ def generate_update_statement(table: str, data_dict: dict, uuid_col_list: list, 
     query = f"""
         UPDATE {table}
         SET {set_string}
-        WHERE UUID_TO_BIN({condition_key}) = '{uuid}';
+        WHERE BIN_TO_UUID({condition_key}) = '{uuid}';
     """
 
     return query, data_tuple, uuid
